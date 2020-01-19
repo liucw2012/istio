df98d510d514b04098c5232a60231eeb13a2b9d0
nicktming <2279563278@qq.com>
2020-01-20 00:19:05 +0800

    add pilot configuration

--- pilot/cmd/pilot-discovery/run.sh
+++ pilot/cmd/pilot-discovery/run.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
 config=""
-config="$config --httpAddr=9090"
+config="$config --httpAddr=:9090"
 config="$config --monitoringAddr=:15014"
 config="$config --log_output_level=default:info"
 config="$config --domain cluster.local"
 config="$config --meshConfig=mesh.yaml"
-config="$config --secureGrpcAddr=''"
 
-./pilot-discovery discovery $config
+./pilot-discovery discovery --secureGrpcAddr="" $config
 
