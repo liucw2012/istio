0991072d80dfb90086bffa2a2237b37b10dd23e9
nicktming <2279563278@qq.com>
2020-01-20 14:59:40 +0800

    add galley configuration

--- galley/cmd/galley/run.sh
+++ galley/cmd/galley/run.sh
@@ -11,6 +11,6 @@ config="$config --validation-webhook-config-file=validating.yaml"
 config="$config --monitoringPort=15015"
 config="$config --log_output_level=default:info"
 config="$config --enable-validation=false"
-
+config="$config --kubeconfig=/root/.kube/config"
 ./galley server --enable-reconcileWebhookConfiguration=false $config
 
