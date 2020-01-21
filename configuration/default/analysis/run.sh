44e80c62c03aab72d97e5f98c9ca1872e692bb0a
nicktming <2279563278@qq.com>
2020-01-21 22:12:48 +0800

    add files

--- configuration/default/analysis/run.sh
+++ configuration/default/analysis/run.sh
@@ -1,10 +1,10 @@
 podname=productpage-v1-8554d58bff-d7j8d
 ns=default
 
-istioctl proxy-config listener listener $podname $ns -o json > lds.json
+istioctl proxy-config listener $podname -n $ns -o json > lds.json
 
-istioctl proxy-config router listener $podname $ns -o json > rds.json
+istioctl proxy-config router $podname -n $ns -o json > rds.json
 
-istioctl proxy-config cluster listener $podname $ns -o json > cds.json
+istioctl proxy-config cluster $podname -n $ns -o json > cds.json
 
-istioctl proxy-config endpoint listener $podname $ns -o json > eds.json
+istioctl proxy-config endpoint $podname -n $ns -o json > eds.json
